9b52d536283eabc924da9688df1801b6f4c07fb4
Anubhav Dhawan <anubhav.dhawan@senecaglobal.net>
2022-03-08 17:42:56 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToDos />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDos from './components/todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    line-height: 1.6;
+    background: #e8f7f0;
+}
+.container{
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2em;
 }
 </style>
---
+++ src/components/todo.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div id="app">
+        <h3>ToDos</h3>
+        <div class="todos">
+            <div class="todo" v-for="todo in allTodos" :key="todo.id">
+                {{todo.title}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    name: "ToDos",
+    computed:{
+       ... mapGetters(['allTodos'])
+    }
+}
+</script>
+
+<style>
+
+</style>
